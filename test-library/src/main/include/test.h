1f7b9fd5b7a284bbe44eae3597499e27a4b93254
Fredrik Orderud <forderud@gmail.com>
2014-06-03 21:04:31 +0200

    Fix build problems on Windows by explicitly exporting/importing "function".

--- test-library/src/main/include/test.h
+++ test-library/src/main/include/test.h
@@ -1,6 +1,16 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
-extern int function(int a);
+#ifdef _WIN32
+#  ifdef CREATE_LIB
+#    define DLL_DECL __declspec(dllexport) // create DLL
+#  else
+#    define DLL_DECL __declspec(dllimport) // use DLL
+#  endif
+#else
+#  define DLL_DECL // not required on other platforms than Windows
+#endif
+
+DLL_DECL int function(int a);
 
 #endif
